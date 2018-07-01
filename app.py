2f64275fec2010d81b3edd16c776b4fa0277bb96
ShivSoni5 <shivsonic05@gmail.com>
2018-07-01 23:49:34 +0530

    repeat name

--- app.py
+++ app.py
@@ -8,5 +8,9 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route('/<name>')
+def hello_name(name):
+    return (f'Hello {name}!')
+
 if __name__ == '__main__':
     app.run()
