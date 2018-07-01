aaae5e8531021d8712f39a5d65768b40e020b967
ShivSoni5 <shivsonic05@gmail.com>
2018-07-02 00:26:00 +0530

    more settings

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python3
 
+import os
 from flask import Flask
 
 app = Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
+print(os.environ['APP_SETTINGS'])
 
 @app.route('/')
 def hello():
---
+++ config.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'Fuck-You'
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+class TestingConfig(Config):
+    TESTING = True
