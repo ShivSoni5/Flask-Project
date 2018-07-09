d537eb9e1f3e4336cc96e45e9b68a314ea172b5f
ShivSoni5 <shivsonic05@gmail.com>
2018-07-10 00:57:44 +0530

    GET and POST

--- app.py
+++ app.py
@@ -1,18 +1,18 @@
 #!/usr/bin/env python3
 
-import os
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
-app.config.from_object(os.environ['APP_SETTINGS'])
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def hello():
-    return "Hello World!"
+    return render_template('index.html')
 
+'''
 @app.route('/<name>')
 def hello_name(name):
     return (f'Hello {name}!')
+'''
 
 if __name__ == '__main__':
     app.run()
