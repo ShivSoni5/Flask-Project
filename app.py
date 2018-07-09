64bafebb893b7517aba36f43498a63f73f033346
ShivSoni5 <shivsonic05@gmail.com>
2018-07-10 01:32:51 +0530

    app.py

--- app.py
+++ app.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python3
 
-from flask import Flask, render_template
+import requests
+from flask import Flask, render_template, request    
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
-def hello():
-    return render_template('index.html')
+def index():
+    errors = []
+    results = {}
+    if request.method == 'POST':
+        try:
+            url = request.form['url']
+            r = requests.get(url)
+            print(r.text)
+        except:
+            errors.append("Unable to get URL. Please make sure it's valid and try again.")
+    return render_template('index.html', errors=errors, results=results)
 
-'''
-@app.route('/<name>')
-def hello_name(name):
-    return (f'Hello {name}!')
-'''
 
 if __name__ == '__main__':
     app.run()
